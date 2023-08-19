e6c31c8750cfe2678dcc2da39de342c3d30ae88f
ENGR-WAJAHAT-AHMED <wajahatahmed621@gmail.com>
2023-08-19 21:44:30 +0500

    make piano buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(PianoApp());
 
-class XylophoneApp extends StatelessWidget {
-  Widget buildKey({required Color color}) {
-    return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () {},
-        child: Container(), // Add an empty container as the child
-      ),
-    );
-  }
-
+class PianoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "piano",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKey(color: Colors.white54),
-              buildKey(color: Colors.black87),
-              buildKey(color: Colors.white54),
-              buildKey(color: Colors.black87),
-              buildKey(color: Colors.white54),
-              buildKey(color: Colors.black87),
-              buildKey(color: Colors.white54),
-              buildKey(color: Colors.black87),
-              buildKey(color: Colors.white54),
-              buildKey(color: Colors.black87),
-              buildKey(color: Colors.white54),
-              buildKey(color: Colors.black87),
-
-            ],
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+            PianoButton(),
+          ],
         ),
       ),
     );
   }
 }
+
+class PianoButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.only(
+            right: 8.0, bottom: 2.0, top: 20),
+        child: RaisedButton(
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
